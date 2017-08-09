7283352db332d9885b7527bb18f1c773b3905378
stories2 <stories2@naver.com>
2017-08-09 10:35:13 +0900

    Navigation module : split xml layout added

--- app/src/main/java/com/machine/i2max/i2max/I2maxMain.java
+++ app/src/main/java/com/machine/i2max/i2max/I2maxMain.java
@@ -1,15 +1,23 @@
 package com.machine.i2max.i2max;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class I2maxMain extends AppCompatActivity {
 
     private TextView mTextMessage;
+    LayoutInflater dynamicLayoutInflater;
+    FrameLayout dynamicAppView;
+    RelativeLayout shareView, graphView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,10 +26,10 @@ public class I2maxMain extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigationShare:
-                    mTextMessage.setText(R.string.ShareData);
+                    VisibleShareView();
                     return true;
                 case R.id.navigationGraph:
-                    mTextMessage.setText(R.string.GraphData);
+                    VisibleGraphView();
                     return true;
                 default:
                     return false;
@@ -35,9 +43,31 @@ public class I2maxMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.i2max_main_layout);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Init();
     }
 
+    public void Init() {
+        dynamicLayoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        dynamicAppView = (FrameLayout) findViewById(R.id.content);
+        shareView = (RelativeLayout) dynamicLayoutInflater.inflate(R.layout.share_data_layout, null);
+        graphView = (RelativeLayout) dynamicLayoutInflater.inflate(R.layout.graph_data_layout, null);
+
+        dynamicAppView.addView(shareView);
+        dynamicAppView.addView(graphView);
+
+        VisibleShareView();
+    }
+
+    public void VisibleShareView() {
+        shareView.setVisibility(View.VISIBLE);
+        graphView.setVisibility(View.INVISIBLE);
+    }
+
+    public void VisibleGraphView() {
+        shareView.setVisibility(View.INVISIBLE);
+        graphView.setVisibility(View.VISIBLE);
+    }
 }
