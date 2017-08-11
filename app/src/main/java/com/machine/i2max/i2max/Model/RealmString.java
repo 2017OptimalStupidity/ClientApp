23b016cabd3c0c61710a1e6b9b0acd3bfba55083
stories2 <stories2@naver.com>
2017-08-11 10:50:30 +0900

    Forcast data model : update date type to string

--- app/src/main/java/com/machine/i2max/i2max/Model/RealmString.java
+++ app/src/main/java/com/machine/i2max/i2max/Model/RealmString.java
@@ -1,22 +1,20 @@
 package com.machine.i2max.i2max.Model;
 
-import java.util.Date;
-
 import io.realm.RealmObject;
 
 /**
  * Created by stories2 on 2017. 8. 11..
  */
 
-public class RealmDate extends RealmObject {
+public class RealmString extends RealmObject {
 
-    public Date date;
+    public String date;
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 }
