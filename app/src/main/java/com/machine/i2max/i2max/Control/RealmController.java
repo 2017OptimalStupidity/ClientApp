22106cfca1f6cf32f0e72f848405e767b5919adc
stories2 <stories2@naver.com>
2017-08-15 19:29:25 +0900

    Realm module : load graph bundle data from database

--- app/src/main/java/com/machine/i2max/i2max/Control/RealmController.java
+++ app/src/main/java/com/machine/i2max/i2max/Control/RealmController.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.machine.i2max.i2max.Model.ForecastDataTable;
+import com.machine.i2max.i2max.Model.RealmDouble;
+import com.machine.i2max.i2max.Model.RealmString;
 import com.machine.i2max.i2max.Model.SellingDataTable;
 
 import java.util.ArrayList;
@@ -11,13 +13,18 @@ import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmList;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
+import static com.machine.i2max.i2max.Settings.DefineManager.BUNDLE_DATE;
+import static com.machine.i2max.i2max.Settings.DefineManager.BUNDLE_RESULT;
+import static com.machine.i2max.i2max.Settings.DefineManager.BUNDLE_STATUS;
 import static com.machine.i2max.i2max.Settings.DefineManager.LOG_LEVEL_ERROR;
 import static com.machine.i2max.i2max.Settings.DefineManager.LOG_LEVEL_INFO;
 import static com.machine.i2max.i2max.Settings.DefineManager.NOT_AVAILABLE;
 import static com.machine.i2max.i2max.Settings.DefineManager.STATUS_WORKING;
+import static com.machine.i2max.i2max.Settings.DefineManager.ZERO;
 import static com.machine.i2max.i2max.Utils.LogManager.PrintLog;
 
 /**
@@ -112,8 +119,44 @@ public class RealmController {
     public Bundle GetLatestForecastData() {
         PrintLog("RealmController", "GetLatestForecastData", "Getting latest forecast data", LOG_LEVEL_INFO);
 
-        int lastProcessId = NOT_AVAILABLE;
-        lastProcessId = GetLatestProcessId();
+        int latestProcessId = NOT_AVAILABLE, i;
+        latestProcessId = GetLatestProcessId();
+
+        try {
+            PrintLog("RealmController", "GetLatestForecastData", "Getting forecast bundle data: " + latestProcessId, LOG_LEVEL_INFO);
+            RealmResults<ForecastDataTable> forecastDataTables = realmInstance.where(ForecastDataTable.class).equalTo("processId", latestProcessId).findAll();
+
+            for(ForecastDataTable indexOfForecastData : forecastDataTables) {
+
+                Bundle forecastBundleData = new Bundle();
+                forecastBundleData.putString(BUNDLE_STATUS, indexOfForecastData.getStatus());
+
+                RealmList<RealmDouble> customRealmDoubleArray = indexOfForecastData.getForecastedData();
+                double[] forecastResult = new double[customRealmDoubleArray.size()];
+
+                for(i = ZERO; i < customRealmDoubleArray.size(); i += 1) {
+                    forecastResult[i] = customRealmDoubleArray.get(i).getRealmDouble();
+                }
+
+                forecastBundleData.putDoubleArray(BUNDLE_RESULT, forecastResult);
+
+                RealmList<RealmString> customRealmStringArray = indexOfForecastData.getForecastedDate();
+                String[] forecastDate = new String[customRealmStringArray.size()];
+
+                for(i = ZERO; i < customRealmStringArray.size(); i += 1) {
+                    forecastDate[i] = customRealmStringArray.get(i).getDate();
+                }
+
+                forecastBundleData.putStringArray(BUNDLE_DATE, forecastDate);
+
+                PrintLog("RealmController", "GetLatestForecastData", "forecast data package rdy", LOG_LEVEL_INFO);
+
+                return forecastBundleData;
+            }
+        }
+        catch (Exception err) {
+            PrintLog("RealmController", "GetLatestForecastData", "Error: " + err.getMessage(), LOG_LEVEL_ERROR);
+        }
         return null;
     }
 }
